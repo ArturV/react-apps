a631c180ca81e092c50fe22ac9720f99b5140b5d
ArturV <volvset@gmail.com>
2023-07-20 13:14:41 +0300

    tailwind config

--- tailwindcss-landing/src/App.js
+++ tailwindcss-landing/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hello</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
     </div>
   );

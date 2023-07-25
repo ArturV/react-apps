8db5dbac8f862bac09b03ac0431ecf7b496ce6d0
ArturV <volvset@gmail.com>
2023-07-25 14:04:28 +0300

    mobile menu header

--- tailwindcss-landing/src/components/Header/index.jsx
+++ tailwindcss-landing/src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as LogoIcon } from "../../images/logo.svg";
+import { ReactComponent as MenuIcon } from "../../images/icon-menu.svg";
 import { Navigation } from "../Navigation";
 import { Menu } from "../Menu";
 import { COMPANY, FEATURES } from "./DataLinks";
@@ -9,7 +10,7 @@ export const Header = () => {
   return (
     <header className="flex items-center">
       <LogoIcon />
-      <nav className="flex space-x-6 ml-8 items-ceter">
+      <nav className="hidden xl:flex space-x-6 ml-8 items-ceter">
         <Navigation text="Feature">
           <Menu items={FEATURES} />
         </Navigation>
@@ -20,9 +21,12 @@ export const Header = () => {
         <Navigation text="About"></Navigation>
       </nav>
 
-      <div className="ml-auto flex space-x-5">
+      <div className="hidden ml-auto xl:flex space-x-5">
         <Button>Login</Button>
         <Button hasBorder="true">Register</Button>
+      </div>
+      <div className="flex xl:hidden ml-auto cursor-pointer z-30">
+        <MenuIcon />
       </div>
     </header>
   );
